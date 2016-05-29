e66e53d77048df378d824ebd13deef8532e97016
ScarpMetal <mpgraham917@gmail.com>
2016-05-29 18:29:14 -0400

    Created first set of models -UserProfile -Conversation -Message

--- RoofMessage/MessageApp/models.py
+++ RoofMessage/MessageApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default='')
+    def __str__(self):
+        return self.name
+
+class Conversation(models.Model):
+    title = models.CharField(max_length=50)
+    users = models.ManyToManyField(UserProfile)
+
+
+class Message(models.Model):
+    date = models.DateField()
+    time = models.TimeField()
+    text = models.CharField(max_length=1000)
+    conversation = models.ForeignKey(Conversation)
